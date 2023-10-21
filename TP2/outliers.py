b82a6be7e0fddd337859218db36d8303f60802b6
FranCire <franciscocirelli@gmail.com>
2023-10-21 19:39:25 -0300

    Finished

--- TP2/outliers.py
+++ TP2/outliers.py
@@ -17,6 +17,9 @@ def tau(n):
     tau = (t_alpha * (n-1))/(sqrt(n) * sqrt(n-2 + t_alpha * t_alpha))
     return tau
 
+def fixed_tau(n):
+    return 1.2
+
 
 def graph_standardized(rtt_between_jumps,filename,title):
     names = [f"{jump[2]} to {jump[3]}" for jump in rtt_between_jumps]
@@ -38,6 +41,7 @@ def graph_standardized(rtt_between_jumps,filename,title):
     plt.close()
 
 
+
 def find_outliers(sample):
     sample_pair = [(x,i) for i,x in enumerate(sample)]
     filtered = []
@@ -48,6 +52,23 @@ def find_outliers(sample):
         idx = sample.index(biggest)
         delta = abs(biggest - mean_val)
         if delta > tau(len(sample)) * std_val:
+            filtered.append(sample_pair[idx][1])
+            del sample[idx]
+            del sample_pair[idx]
+        else:
+            return filtered
+    return filtered
+
+def find_outliers_fixed(sample):
+    sample_pair = [(x,i) for i,x in enumerate(sample)]
+    filtered = []
+    while len(sample) >= 3:
+        mean_val = mean(sample)
+        std_val = std(sample)
+        biggest = max(sample)
+        idx = sample.index(biggest)
+        delta = abs(biggest - mean_val)
+        if delta > fixed_tau(len(sample)) * std_val:
             filtered.append(sample_pair[idx][1])
             del sample[idx]
             del sample_pair[idx]
@@ -82,4 +103,35 @@ if __name__ == "__main__":
     outlier_jumps = [rtt_between_jumps[i] for i in outliers]
     write_object_to_file(outlier_jumps,"data/tokyo_outliers.txt")
     graph_standardized(rtt_between_jumps,"data/standardized_tokyo","Sample Estandarizada Tokyo")
+
+    rtt_between_jumps = read_object_from_file("/home/fcirelli/Documents/UBA/Redes/TPs/TP2/rtt_between_jumps_cam.ac.uk.txt")
+    outliers = find_outliers_fixed([jump[4] for jump in rtt_between_jumps])
+    outlier_jumps = [rtt_between_jumps[i] for i in outliers]
+    print(len(outlier_jumps))
+    write_object_to_file(outlier_jumps,"data/cambridge_fixed_outliers.txt")
+
+
+    rtt_between_jumps = read_object_from_file("/home/fcirelli/Documents/UBA/Redes/TPs/TP2/rtt_between_jumps_jhu.edu.txt")
+    outliers = find_outliers_fixed([jump[4] for jump in rtt_between_jumps])
+    outlier_jumps = [rtt_between_jumps[i] for i in outliers]
+    print(len(outlier_jumps))
+
+    write_object_to_file(outlier_jumps,"data/jhu_fixed_outliers.txt")
+
+
+    rtt_between_jumps = read_object_from_file("/home/fcirelli/Documents/UBA/Redes/TPs/TP2/rtt_between_jumps_uj.ac.za.txt")
+    outliers = find_outliers_fixed([jump[4] for jump in rtt_between_jumps])
+    outlier_jumps = [rtt_between_jumps[i] for i in outliers]
+    print(len(outlier_jumps))
+
+    write_object_to_file(outlier_jumps,"data/johannesburg_fixed_outliers.txt")
+
+
+    rtt_between_jumps = read_object_from_file("/home/fcirelli/Documents/UBA/Redes/TPs/TP2/rtt_between_jumps_www.u-tokyo.ac.jp.txt")
+    outliers = find_outliers_fixed([jump[4] for jump in rtt_between_jumps])
+    outlier_jumps = [rtt_between_jumps[i] for i in outliers]
+    print(len(outlier_jumps))
+
+    write_object_to_file(outlier_jumps,"data/tokyo_fixed_outliers.txt")
+    
 
